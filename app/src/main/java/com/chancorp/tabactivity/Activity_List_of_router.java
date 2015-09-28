2fa31051b00ba2431c190a396eb8c8b8e1efe009
Koedachan <kim024680@naver.com>
2015-09-28 18:48:01 +0900

    NO

--- app/src/main/java/com/chancorp/tabactivity/Activity_List_of_router.java
+++ app/src/main/java/com/chancorp/tabactivity/Activity_List_of_router.java
@@ -1,11 +1,22 @@
 package com.chancorp.tabactivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -30,11 +41,6 @@ public class Activity_List_of_router extends Activity implements View.OnClickLis
     }
 
     @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-    }
-
-    @Override
     public void onClick(View v) {
         if(v.getId() == R.id.addrouterbtn) {
             startActivityForResult(new Intent(this, Activity_add_router.class), REQUEST_CODE);
@@ -51,4 +57,106 @@ public class Activity_List_of_router extends Activity implements View.OnClickLis
             }
         }
     }
+
+    public void datastechanged(int position) {
+        arraylist.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class CustomAdapter01 extends BaseAdapter implements View.OnClickListener {
+
+        int mResource;
+        ArrayList<String> mObject;
+        LayoutInflater minflater;
+        private Context context;
+
+        public CustomAdapter01(Context context,int layoutId, ArrayList<String> object) {
+            this.context = context;
+            mResource = layoutId;
+            mObject = object;
+            minflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            try {
+                return mObject.size();
+            } catch(NullPointerException e) {return 0;}
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mObject.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return (long)position;
+        }
+        @Override
+        public View getView(int position, View convertview, ViewGroup parent) {
+            TextView text1;
+            ImageView img1, img2;
+            final int position_ = position;
+            final Context context = parent.getContext();
+
+            // 아이템 뷰 생성 및 재사용
+            if(convertview == null) {
+                convertview = minflater.inflate(mResource, parent, false);
+
+                //UI 레퍼런스 가져오기
+                text1 = (TextView) convertview.findViewById(R.id.customadapter01text01);
+                img1 = (ImageView) convertview.findViewById(R.id.customadapter01img01);
+                img2 = (ImageView) convertview.findViewById(R.id.customadapter01img02);
+
+                text1.setText((String) getItem(position));
+                img1.setImageResource(R.mipmap.ic_launcher);
+
+                Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), R.drawable.delete);
+                Bitmap resized = Bitmap.createScaledBitmap(bmp, 150, 150, true);
+                img2.setImageBitmap(resized);
+
+            img2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public  void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("Caution");
+                    Log.d("Activity_List_of_router", String.valueOf(position_));
+                            builder.setMessage("Erase wi-fi " + arraylist.get(position_) + "?")
+                            .setCancelable(true)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    datastechanged(position_);
+                                    dialog.cancel();
+                                }
+                            })
+                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog dialog_ = builder.create();
+                    dialog_.show();
+                }
+            });
+
+            }
+            return convertview;
+        }
+        @Override
+        public void onClick(View v) {}
+    }
+
 }
