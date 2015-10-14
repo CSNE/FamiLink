4649e7bcf47f7a4ece92b097b0c06fa9b636eb4c
Koedachan <kim024680@naver.com>
2015-10-14 17:59:49 +0900

    Chansol Yang ~ is working.

--- app/src/main/java/com/chancorp/tabactivity/Page5Settings.java
+++ app/src/main/java/com/chancorp/tabactivity/Page5Settings.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.SwitchPreference;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
@@ -23,11 +24,6 @@ import java.io.StringWriter;
 public class Page5Settings extends Fragment implements RedrawableFragment{
 
     FamilyData fd;
-    Switch ServiceSwitch;
-    SharedPreferences setting;
-    SharedPreferences.Editor settingeditor;
-    final int defValue = 1234567;
-
     @Override
     public void onAttach(Activity a){
         super.onAttach(a);
@@ -45,35 +41,7 @@ public class Page5Settings extends Fragment implements RedrawableFragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.page_5_settings, container, false);
-
-        setting = this.getActivity().getSharedPreferences("setting", 0);
-        settingeditor = setting.edit(); // Ready to edit sharedpreferences.
-
-        ServiceSwitch = (Switch) rootView.findViewById(R.id.slidingtoggleswitch);
-        ServiceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
-                if(isChecked) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                    builder.setTitle("Caution");
-                    builder.setMessage("Service is Going to Start. By pressing the switch again, you can turn off switch.")
-                            .setCancelable(false)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int whichButton) {
-                                    dialog.cancel();
-                                }
-                            });
-                    AlertDialog dialog_ = builder.create();
-                    dialog_.show();
-                    getActivity().startService(new Intent(getContext(), Service_Lockscreen.class));
-                    settingeditor.putBoolean("ServiceRunning", true);
-                } else {
-                    getActivity().stopService(new Intent(getContext(), Service_Lockscreen.class));
-                    settingeditor.putBoolean("ServiceRunning", false);
-                }
-                settingeditor.commit();
-            }
-        });
+        startActivity(new Intent(this.getActivity(), Activity_Page5Settings.class));
         return rootView;
     }
 
