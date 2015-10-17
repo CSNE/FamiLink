ac7865f3f71f7b4d7f2c4d407684bb89f585025f
Koedachan <kim024680@naver.com>
2015-10-17 17:20:28 +0900

    Fuxxing Git!

--- app/src/main/java/com/chancorp/tabactivity/Activity_Lockscreen.java
+++ app/src/main/java/com/chancorp/tabactivity/Activity_Lockscreen.java
@@ -20,83 +20,13 @@ import android.widget.TextView;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Activity_Lockscreen extends AppCompatActivity implements Runnable {
-
-    Handler hand = null;
-    ImageView img;
-    Button btn;
-    TextView textview;
-    Timer timer;
-    TimerTask timertask = null;
-    static boolean AlreadyTimer = false;
+public class Activity_Lockscreen extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lock_screen);
-        img = (ImageView)findViewById(R.id.imgview);
-        img.setImageResource(R.drawable.flower1);
-        btn = (Button) findViewById(R.id.dislock);
-        textview = (TextView) findViewById(R.id.MacAddressText);
-        hand = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                if(msg.what == 1) {
-                    WifiManager wifimanager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-                    WifiInfo wifinfo = wifimanager.getConnectionInfo();
-                    textview.setText("Router Address : " + wifinfo.getBSSID());
-                } else if(msg.what == 2) {
-                    textview.setText("Wifi is off, now.");
-                } else if(msg.what == 3) {
-                    textview.setText("Your are Runing 3G DATA!");
-                }
-            }
-        };
-        textStart();
-    }
-
-    public void textStart() {
-        if(AlreadyTimer) return;
-        AlreadyTimer = true;
-        timertask = new TimerTask() {
-            @Override
-            public void run() {
-                Log.d("MainActivity_1_1", "Timer is Running");
-                UpdateText();
-            }
-        };
-        timer = new Timer();
-        timer.schedule(timertask, 1000, 2000);
-    }
-
-    private void UpdateText() {
-        ConnectivityManager cntmng = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo nwi = cntmng.getActiveNetworkInfo();
-        if(nwi == null) {
-            hand.sendEmptyMessage(2);
-        }
-        else {
-            if (nwi.getType() == 0) {
-                hand.sendEmptyMessage(3);
-            } else {
-                WifiManager wifimanager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-                if (wifimanager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
-                    hand.sendEmptyMessage(1);
-                } else {
-                    hand.sendEmptyMessage(2);
-                }
-            }
-        }
-        return;
-    }
-
-    public void dislockonClick(View v) {
-        //Receiver_WifiStateChange.AlreadyRunning = false;
-        if(timertask != null) {
-            timer.cancel();
-            AlreadyTimer = false;
-        }
-        finish();
+        
     }
 
     @Override
@@ -120,7 +50,4 @@ public class Activity_Lockscreen extends AppCompatActivity implements Runnable {
 
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void run() {}
 }
