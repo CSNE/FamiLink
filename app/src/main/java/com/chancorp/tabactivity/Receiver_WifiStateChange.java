3f85df997465eda83830c8c41605ddf179f62ca8
Koedachan <kim024680@naver.com>
2015-10-18 23:35:08 +0900

    Sexxing Git!

--- app/src/main/java/com/chancorp/tabactivity/Receiver_WifiStateChange.java
+++ app/src/main/java/com/chancorp/tabactivity/Receiver_WifiStateChange.java
@@ -20,44 +20,42 @@ public class Receiver_WifiStateChange extends BroadcastReceiver {
     boolean electronics = false;
     Timer mTimer = null;
     TimerTask mTimerTask = null;
+    String lastestssid = null, lastestbssid = null;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (mPref == null) mPref = PreferenceManager.getDefaultSharedPreferences(context);
         if(edit == null) edit = mPref.edit();
         electronics = mPref.getBoolean("familink_Electronics", false);
-        WifiManager wifimanager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if(lastestssid ==  null) lastestssid = "";
+        if(lastestbssid == null) lastestbssid = "";
 
+        WifiManager wifimanager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         String WIFI1 = new String();
         WIFI1 = context.getResources().getString(R.string.Wifi_change1);
         String WIFI2 = new String();
         WIFI2 = context.getResources().getString(R.string.Wifi_change2);
 
         if (intent.getAction().equals(WIFI1) || intent.getAction().equals(WIFI2)) {
-
-            Log.d("!!", String.valueOf(wifimanager.getWifiState()) + "   " + mPref.getString("familink_lastest_BSSID", "undefined"));
-
             if(wifimanager.getWifiState() == WifiManager.WIFI_STATE_DISABLED) { // disabled
                 WifiInfo wifinfo = wifimanager.getConnectionInfo();
-                if(mPref.getString("familink_lastest_BSSID", "undefined").equals("undefined") == false) {
+                if(lastestssid.equals("undefined") == false) {
                     Log.d("disabled", "is there server delay?");
-                    boolean decision = ServerComms.fd.matchRouter(new RouterInformation(mPref.getString("familink_lastest_SSID",""),mPref.getString("familink_lastest_BSSID","")));
+                    boolean decision = ServerComms.fd.matchRouter(new RouterInformation(lastestssid,lastestbssid));
                     Log.d("Decision  :  ", String.valueOf(decision));
                     ServerComms sc = new ServerComms(context);
                     sc.updateStatus(new RouterInformation(wifinfo.getSSID(), wifinfo.getBSSID()), electronics, decision, context);
-                    edit.putString("familink_lastest_BSSID", "undefined");
-                    edit.putString("familink_lastest_SSID", "undefined");
-                    edit.commit();
+                    lastestssid = "undefined";
+                    lastestbssid = "undefined";
                 }
             }
 
             else if(wifimanager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) { // enabled
                 WifiInfo wifinfo = wifimanager.getConnectionInfo();
                 try {
-                    if(wifinfo.getBSSID().equals(mPref.getString("familink_lastest_BSSID","undefined")) == false) {
-                        edit.putString("familink_lastest_BSSID", wifinfo.getBSSID());
-                        edit.putString("familink_lastest_SSID", wifinfo.getSSID());
-                        edit.commit();
+                    if(lastestbssid.equals("undefined") == false) {
+                        lastestssid = wifinfo.getSSID();
+                        lastestbssid = wifinfo.getBSSID();
                         Log.d("enabled", "is there server delay?  " + wifinfo.getBSSID());
                         ServerComms sc = new ServerComms(context);
                         sc.updateStatus(new RouterInformation(wifinfo.getSSID(), wifinfo.getBSSID()), electronics, false, context);
